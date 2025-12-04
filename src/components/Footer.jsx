8f6804d1c458dcfc5111b3e399745a85473d35df
Abhi <abhijith2002verengal@gmail.com>
2025-12-05 00:10:51 +0530

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,6 +19,10 @@ export default function Footer() {
             <Link to="/" className="flex items-center gap-2">
                   <span className="font-serif text-xl text-naseema"><img src={logo} alt="" width="200" height="200" viewBox="0 0 24 24" className="text-naseema"/></span>
             </Link>
+            {/* <div>
+              <div className="font-serif text-2xl text-naseema">Naseema</div>
+              <div className="text-sm text-gray-600">Fine Jewellery</div>
+            </div> */}
           </div>
           <p className="text-sm text-gray-600">Handcrafted in India. Designed with love and care to become your family heirlooms.</p>
         </div>
@@ -26,6 +30,14 @@ export default function Footer() {
         <div>
           <h4 className="font-medium mb-3">Location</h4>
           <p className="text-sm text-gray-600">Up Hill, Malappuram,<br/>Kerala, India</p>
+
+          <div className="mt-4">
+            <h4 className="font-medium mb-2">Contact</h4>
+            <p className="text-sm text-gray-600">
+              <a href="mailto:info@naseema.gold" className="hover:text-naseema">info@naseema.gold</a><br/>
+              <a href="tel:+919876543210" className="hover:text-naseema">+91 98765 43210</a>
+            </p>
+          </div>
         </div>
 
         <div>
@@ -33,23 +45,23 @@ export default function Footer() {
           <ul className="text-sm text-gray-600 space-y-2">
             <li><Link to="/" className="hover:text-naseema">Home</Link></li>
             <li><Link to="/products" className="hover:text-naseema">Products</Link></li>
-            <li><Link to="/gallery" className="hover:text-naseema">Gallery</Link></li>
             <li><Link to="/about" className="hover:text-naseema">About</Link></li>
             <li><Link to="/contact" className="hover:text-naseema">Contact</Link></li>
+            <li><Link to="/gallery" className="hover:text-naseema">Gallery</Link></li>
           </ul>
         </div>
 
         <div>
           <h4 className="font-medium mb-3">Follow Us</h4>
           <div className="flex gap-3 mb-4">
-            <SocialIcon href="https://www.instagram.com/naseemagold/?hl=en">IG</SocialIcon>
-            <SocialIcon href="https://www.facebook.com/naseemagoldanddiamonds/">FB</SocialIcon>
+            <SocialIcon href="https://instagram.com">IG</SocialIcon>
+            <SocialIcon href="https://facebook.com">FB</SocialIcon>
             <SocialIcon href="https://pinterest.com">PT</SocialIcon>
           </div>
           <h4 className="font-medium mb-2">Newsletter</h4>
           <form className="flex gap-2">
             <input className="px-3 py-2 border rounded w-full" placeholder="Your email" />
-            <button className="px-4 py-2 bg-naseema border-1 hover:to-blue-300 text-[#000] rounded">Subscribe</button>
+            <button className="px-4 py-2 bg-naseema text-white rounded">Subscribe</button>
           </form>
         </div>
       </div>
