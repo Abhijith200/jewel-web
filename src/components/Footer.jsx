8431d5b9de08365ac41b4197a8cea83e092b3ee6
Abhi <abhijith2002verengal@gmail.com>
2025-12-16 14:31:39 +0530

    changed logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/elvinbermin.png'
+import logo from '../assets/logo.png'
 
 function SocialIcon({ href, children }) {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import logo from '../assets/elvinbermin.png'
+import logo from '../assets/logo.png'
 
 function Logo() {
   return (
